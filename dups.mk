bb3b6d0124ce7f40fa816eeebcd633e603a16c88
Thierry Delamare <t.delamare@laposte.net>
2019-12-27 19:20:00 +0100

    Fixes missing deps to oxa-duplicity.jsonnet

--- dups.mk
+++ dups.mk
@@ -11,16 +11,17 @@ top:; @date
 .PHONY: top dirs main
 
 dir := tmp
-jsonnet := local nodes = import 'ext/data-nodes/oxa-duplicity.jsonnet';
+nodes := ext/data-nodes/oxa-duplicity.jsonnet
+jsonnet := local nodes = import '$(nodes)';
 jsonnet += 'mkdir -p $(dir); proot -w $(dir) mkdir -p ' + std.join(' ', nodes.dups.nodes)
 dirs := $(dir)/.dirs
 $(dirs) = jsonnet -Se "$(jsonnet)" | tee /dev/stderr | dash && touch $@
-$(dirs): $(self); $($@)
+$(dirs): $(self) $(nodes); $($@)
 dirs: $(dirs)
 
 repo != git config remote.origin.url
 
 stone := $(dir)/.stone
-$(stone): dups.jsonnet dups.libsonnet $(self); jsonnet -V repo=$(repo) -m $(@D) -S $< && touch $@
+$(stone): dups.jsonnet dups.libsonnet $(nodes) $(self); jsonnet -V repo=$(repo) -m $(@D) -S $< && touch $@
 
 main: dirs $(stone)
